0eb9c615874e1b64ad73086b43298352e2f80f44
Nathanael <nathanael@CS-571.local>
2018-06-22 11:58:33 -0300

    Fix ensure ascii to false

--- main.py
+++ main.py
@@ -14,16 +14,7 @@ platforms = parameters.get('platforms')
 pageGeneralDatas = GeneralDatas(path_to_features_folder, path_to_contract_tests_folder, total_endpoints_used, platforms)
 pageGeneralDatas.set_functional_datas_of_project()
 
-# print "HEAD"
-# print pageGeneralDatas.project_functional_coverage
-# print pageGeneralDatas.total_scenarios_of_project
-# print pageGeneralDatas.project_contract_coverage()
-# print pageGeneralDatas.get_total_number_endpoints_tested()
-
 datas = {} 
-
-
-
 datas['total_endpoints_tested'] = ""
 datas['contract_test_coverage'] = ""
 datas['total_scenarios'] = ""
@@ -36,8 +27,6 @@ datas['total_scenarios'] = (pageGeneralDatas.total_scenarios_of_project)
 datas['contract_test_coverage'] = (pageGeneralDatas.project_contract_coverage())
 datas['total_endpoints_tested'] = (pageGeneralDatas.get_total_number_endpoints_tested())
 
-
-# # print "FEATURES"
 for feature in pageGeneralDatas.array_features:
     datas['features'].append({
         'feature_name': feature.feature_name,
@@ -47,15 +36,5 @@ for feature in pageGeneralDatas.array_features:
         'scenarios_not_implemented': feature.array_scenarios_not_implemented,
         'coverage': feature.get_coverage()
     })
-    # print feature.feature_name
-    # print feature.get_total_number_scenarios_implemented()
-    # print feature.get_total_number_scenarios()
-    # print "IMPLEMENTED"
-    # for scenario in feature.array_scenarios_implemented:
-    #     print scenario
-    # print "NOT IMPLEMENTED"
-    # for scenario in feature.array_scenarios_not_implemented:
-    #     print scenario
-    # print feature.get_coverage()
 with open('generalDatas.json', 'w') as outfile:  
-    json.dump(datas, outfile)
+    json.dump(datas, outfile, ensure_ascii=False)
