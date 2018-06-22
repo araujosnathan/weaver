478dba74f08b18b92be95c4242c76dcce3e7e4b7
Nathanael <nathanael@CS-571.local>
2018-06-22 03:16:33 -0300

    Get contract test datas

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import json
 from platform.general_datas_class import GeneralDatas 
 config_file = open('config.yml')
 
 parameters = yaml.load(config_file)
 path_to_features_folder = parameters.get('path_to_features')
+path_to_contract_tests_folder = parameters.get('path_to_contract_tests')
+total_endpoints_used = parameters.get('total_endpoints_used')
 platforms = parameters.get('platforms')
 
-pageGeneralDatas = GeneralDatas(path_to_features_folder, platforms)
+pageGeneralDatas = GeneralDatas(path_to_features_folder, path_to_contract_tests_folder, total_endpoints_used, platforms)
 pageGeneralDatas.set_functional_datas_of_project()
+
+# print "HEAD"
+# print pageGeneralDatas.project_functional_coverage
+# print pageGeneralDatas.total_scenarios_of_project
+# print pageGeneralDatas.project_contract_coverage()
+# print pageGeneralDatas.get_total_number_endpoints_tested()
+
+datas = {} 
+
+
+
+datas['total_endpoints_tested'] = ""
+datas['contract_test_coverage'] = ""
+datas['total_scenarios'] = ""
+datas['functional_coverage'] = ""
+datas['features'] = []
+
+
+datas['functional_coverage'] = (pageGeneralDatas.project_functional_coverage)
+datas['total_scenarios'] = (pageGeneralDatas.total_scenarios_of_project)
+datas['contract_test_coverage'] = (pageGeneralDatas.project_contract_coverage())
+datas['total_endpoints_tested'] = (pageGeneralDatas.get_total_number_endpoints_tested())
+
+
+# # print "FEATURES"
 for feature in pageGeneralDatas.array_features:
-    print "FEATURE"
-    print feature.feature_name
-    print feature.get_total_number_scenarios_implemented()
-    print feature.get_total_number_scenarios()
-    print "SCEN .. IMPLEMENTED"
-    for scenario in feature.array_scenarios_implemented:
-        print scenario
-    print "SCEN .. NOT IMPLEMENTED"
-    for scenario in feature.array_scenarios_not_implemented:
-        print scenario
-    print feature.get_coverage()
-print "PROJECT"
-print pageGeneralDatas.project_functional_coverage
-print pageGeneralDatas.total_scenarios_of_project
+    datas['features'].append({
+        'feature_name': feature.feature_name,
+        'number_scenarios_implemented': feature.get_total_number_scenarios_implemented(),
+        'total_number_scenarios': feature.get_total_number_scenarios(),
+        'scenarios_implemented': map(str,feature.array_scenarios_implemented),
+        'scenarios_not_implemented': feature.array_scenarios_not_implemented,
+        'coverage': feature.get_coverage()
+    })
+    # print feature.feature_name
+    # print feature.get_total_number_scenarios_implemented()
+    # print feature.get_total_number_scenarios()
+    # print "IMPLEMENTED"
+    # for scenario in feature.array_scenarios_implemented:
+    #     print scenario
+    # print "NOT IMPLEMENTED"
+    # for scenario in feature.array_scenarios_not_implemented:
+    #     print scenario
+    # print feature.get_coverage()
+with open('generalDatas.json', 'w') as outfile:  
+    json.dump(datas, outfile)
--- platform/general_datas_class.py
+++ platform/general_datas_class.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
-
 import os, fnmatch, re
 from functional_datas_class import FunctionalDatas
 
 class GeneralDatas:
     path_to_features_folder = ""
+    path_to_contract_tests_folder = ""
     platform = ""
     total_scenarios_implemented_of_project = 0
     total_scenarios_of_project = 0
     project_functional_coverage = 0
+    total_endpoints_used = 0
+    project_contract_coverage = 0
+    total_number_of_endpoints = 0
     array_features = []
+    array_endpoints = []
 
-    def __init__(self, path_to_features_folder, platform):
+    def __init__(self, path_to_features_folder, path_to_contract_tests_folder, total_endpoints_used, platform):
         self.path_to_features_folder = path_to_features_folder
+        self.path_to_contract_tests_folder = path_to_contract_tests_folder
         self.platform = platform
+        self.total_endpoints_used = total_endpoints_used
 
     def get_features_from_project(self):
         list_of_feature_files = os.listdir(self.path_to_features_folder)
@@ -29,7 +35,7 @@ class GeneralDatas:
         array_matches = ['Funcionalidade','Feature']
         for line in feature_file.readlines():
             if any(re.findall(r"(?=("+'|'.join(array_matches)+r"))", line)):
-                feature_name = line.split(':')[1]
+                feature_name = line.split(':')[1].strip()
         return feature_name
 
     def get_scenario_names_implemented_by_feature(self, path_to_feature_file):
@@ -37,7 +43,7 @@ class GeneralDatas:
         with open(path_to_feature_file) as feature_file:
             for line in feature_file:
                 if any(re.findall(r"%s" % self.platform, line)):
-                    array_scenario_names_implemented.append(next(feature_file).split(':')[1])
+                    array_scenario_names_implemented.append(next(feature_file).split(':')[1].strip())
         return array_scenario_names_implemented
 
     def get_scenario_names_by_feature(self, path_to_feature_file):
@@ -46,7 +52,7 @@ class GeneralDatas:
         array_matches = ['Cenário','Cenario','Scenario']
         for line in feature_file.readlines():
             if any(re.findall(r"(?=("+'|'.join(array_matches)+r"))", line)):
-                array_scenario_names.append(line.split(':')[1])
+                array_scenario_names.append(line.split(':')[1].strip())
         return array_scenario_names
 
     def set_functional_data_by_feature(self):
@@ -69,4 +75,50 @@ class GeneralDatas:
             self.total_scenarios_implemented_of_project = self.total_scenarios_implemented_of_project + feature.get_total_number_scenarios_implemented()
             self.total_scenarios_of_project = self.total_scenarios_of_project + feature.get_total_number_scenarios()
         self.project_functional_coverage = (self.total_scenarios_implemented_of_project*100.0)/self.total_scenarios_of_project
-                
+    
+    def get_contract_tests_from_project(self):
+        list_of_contract_tests_files = os.listdir(self.path_to_contract_tests_folder)
+        file_pattern = "*testing*"
+        array_contract_tests_files = []
+        for file_in_dir in list_of_contract_tests_files:  
+            if fnmatch.fnmatch(file_in_dir, file_pattern):
+                array_contract_tests_files.append(file_in_dir)
+        return array_contract_tests_files
+
+    def set_endpoints_from_file(self, path_to_contract_tests_file):
+        contract_tests_file = open(path_to_contract_tests_file, 'r')
+        for line in contract_tests_file.readlines():
+            if any(re.findall(r"const PATH", line)):
+                self.array_endpoints.append(line.split("=")[0])
+
+    def remove_duplicates(self, array_duplicate):
+        unique_endpoints = []
+        for endpoint in array_duplicate:
+            if endpoint not in unique_endpoints:
+                unique_endpoints.append(endpoint)
+        return unique_endpoints
+
+    def remove_white_spaces(self, array):
+        unique_endpoints = []
+        for endpoint in array:
+            unique_endpoints.append(endpoint.replace(' ', ''))
+        return unique_endpoints
+
+    def get_total_endpoints_from_project(self):
+        for each_file in self.get_contract_tests_from_project():
+            self.set_endpoints_from_file(self.path_to_contract_tests_folder + each_file)
+        
+        self.array_endpoints = self.remove_white_spaces(self.array_endpoints)
+        self.array_endpoints = self.remove_duplicates(self.array_endpoints)
+        return self.array_endpoints
+
+    def get_total_number_endpoints_tested(self):
+        self.total_number_of_endpoints = 0
+        self.get_total_endpoints_from_project()
+        self.total_number_of_endpoints = self.total_number_of_endpoints + len(self.array_endpoints)
+        return self.total_number_of_endpoints
+
+    def project_contract_coverage(self):
+        self.project_contract_coverage = (self.get_total_number_endpoints_tested()*100.0)/self.total_endpoints_used
+        return self.project_contract_coverage
+    
