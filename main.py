5a4d9b38402421442f33bc2f119f03aa86bf29e4
Nathanael <nathanael@CS-571.local>
2018-06-25 21:09:54 -0300

    Add language support

--- main.py
+++ main.py
@@ -5,39 +5,69 @@ import json
 import shutil
 from platform.general_datas_class import GeneralDatas 
 from platform.generate_generalDatas_class import GenerateGeneralDatas 
-config_file = open('config.yml')
 
-parameters                          = yaml.load(config_file)
-path_to_features_folder             = parameters.get('path_to_features')
-path_to_contract_tests_folder       = parameters.get('path_to_contract_tests')
-total_endpoints_used                = parameters.get('total_endpoints_used')
-path_unit_test_ios                  = parameters.get('ios_unit_test_report')
-path_unit_test_android              = parameters.get('android_unit_test_report')
-platforms                           = parameters.get('platforms')
-report_name                         = parameters.get('report_name')
+import sys, getopt
+
+def main(argv):
+    global lang
+    lang = ''
+    teams = 1
+    if (len(argv) < 1):
+        print 'main.py -l <language> -t <number_of_teams>'
+        sys.exit(2)
+    else:
+        try:
+            opts, args = getopt.getopt(argv,"l:t:",["lang=","teams="])
+        except getopt.GetoptError:
+            print 'main.py -l <language> -t <number_of_teams>'
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt == '-h':
+                print 'main.py [-l, --lang] <language> | [-t,--teams <outputfile>'
+                sys.exit()
+            elif opt in ("-l", "--lang"):
+                lang = arg
+            elif opt in ("-t", "--teams"):
+                teams = arg
+
+def weaver():
+    config_file = open('config.yml')
+    parameters                          = yaml.load(config_file)
+    path_to_features_folder             = parameters.get('path_to_features')
+    path_to_contract_tests_folder       = parameters.get('path_to_contract_tests')
+    total_endpoints_used                = parameters.get('total_endpoints_used')
+    path_unit_test_ios                  = parameters.get('ios_unit_test_report')
+    path_unit_test_android              = parameters.get('android_unit_test_report')
+    platforms                           = parameters.get('platforms')
+    report_name                         = parameters.get('report_name')
 
 
-dashboard_menu = {}
-dashboard_menu['menu_platforms'] = []
-sprint_platforms = {}
-sprint_platforms['platforms'] = []
+    dashboard_menu = {}
+    dashboard_menu['menu_platforms'] = []
+    dashboard_menu['language'] = ""
+    dashboard_menu['language'] = lang
+    sprint_platforms = {}
+    sprint_platforms['platforms'] = []
 
-for platform in platforms.split(','):
-    platform = platform.strip()
-    pageGeneralDatas = GeneralDatas(path_to_features_folder, path_to_contract_tests_folder, total_endpoints_used, path_unit_test_ios, path_unit_test_android, platform)
-    generateGeneralDatas = GenerateGeneralDatas(pageGeneralDatas, platform, report_name)
+    for platform in platforms.split(','):
+        platform = platform.strip()
+        pageGeneralDatas = GeneralDatas(path_to_features_folder, path_to_contract_tests_folder, total_endpoints_used, path_unit_test_ios, path_unit_test_android, platform)
+        generateGeneralDatas = GenerateGeneralDatas(pageGeneralDatas, platform, report_name)
 
-    sprint_datas = generateGeneralDatas.set_dashboard_by_platform()
-    sprint_platforms['platforms'].append(sprint_datas)
-    generateGeneralDatas.set_sprint_metrics_dashbord_by_platform()
-    dashboard_menu['menu_platforms'].append(platform)
-    shutil.copy2('template/index.html', 'template/index-' + platform + '.html')
+        sprint_datas = generateGeneralDatas.set_dashboard_by_platform()
+        sprint_platforms['platforms'].append(sprint_datas)
+        generateGeneralDatas.set_sprint_metrics_dashbord_by_platform()
+        dashboard_menu['menu_platforms'].append(platform)
+        shutil.copy2('template/index.html', 'template/index-' + platform + '.html')
 
-sprint_platforms = json.dumps(sprint_platforms, ensure_ascii=False)
-with open('template/datas/generalDatas.json', 'w') as outfile: 
-            outfile.write("data = '" +  str(sprint_platforms) + "'")
+    sprint_platforms = json.dumps(sprint_platforms, ensure_ascii=False)
+    with open('template/datas/generalDatas.json', 'w') as outfile: 
+                outfile.write("data = '" +  str(sprint_platforms) + "'")
 
-dashboard_menu = json.dumps(dashboard_menu, ensure_ascii=False)
-with open('template/datas/dashboardMenu.json', 'w') as outfile:
-    outfile.write("dashboard_menu = '" +  str(dashboard_menu) + "'")
+    dashboard_menu = json.dumps(dashboard_menu, ensure_ascii=False)
+    with open('template/datas/dashboardMenu.json', 'w') as outfile:
+        outfile.write("dashboard_menu = '" +  str(dashboard_menu) + "'")
 
+
+main(sys.argv[1:])
+weaver()
--- template/js/load-datas/load-dashboard-menu.js
+++ template/js/load-datas/load-dashboard-menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var dashboord = JSON.parse(dashboard_menu);
+    console.log(langJSON.languages[0].tag)
     for(var i = 0; i < dashboord.menu_platforms.length; i++){
         document.getElementById('REPORT_PLATFORM_NAME').insertAdjacentHTML('beforeend', "<li><a href='index-" + dashboord.menu_platforms[i] + ".html'>"+ dashboord.menu_platforms[i] +"</a></li>")
     }
