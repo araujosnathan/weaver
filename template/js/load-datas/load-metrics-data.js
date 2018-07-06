7f2c5d8dfc7aae478d1899cca04cfe0617e4d0fd
nathanael.araujo <nathanael.araujo@concrete.com.br>
2018-07-06 19:16:28 -0300

    Add dropdown menu to see sprint report

--- template/js/load-datas/load-metrics-data.js
+++ template/js/load-datas/load-metrics-data.js
@@ -6,128 +6,102 @@ $(document).ready(function() {
     let keys = Object.keys(sprintsTableJSON).sort()
     
     var set_dashboard = function(key_plat, key){
-        // console.log(key_plat)
-        // console.log(Object.keys(sprintsTableJSON[key].platforms).length)
-        // obj = sprintsTableJSON[key].platforms['android'].functional_coverage
-        // console.log(obj)
-        // platform = Object.keys(sprintsTableJSON[key].platforms)[0]
-        // console.log(platform)
-
-
         for(var i = 0; i < Object.keys(sprintsTableJSON[key].platforms).length; i ++){   
             if(key_plat.includes(Object.keys(sprintsTableJSON[key].platforms)[i])){
-                // console.log("SETA " + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].functional_coverage)
                 document.getElementById('FUNCTIONAL_PERCENTAGE').innerHTML = sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].functional_coverage + "%";
-                // document.getElementById('FUNCTIONAL_SCENARIOS_NUMBER').innerHTML = generalDatas.platforms[i].total_scenarios;
-                // if(generalDatas.platforms[i].contract_test_coverage == "N/A"){
-                //     document.getElementById('CONTRACT_PERCENTAGE').innerHTML = generalDatas.platforms[i].contract_test_coverage;
-                // }
-                // else{
-                //     document.getElementById('CONTRACT_PERCENTAGE').innerHTML = generalDatas.platforms[i].contract_test_coverage + "%";
-                // }
-                // document.getElementById('ENDPOINTS_NUMBER').innerHTML = generalDatas.platforms[i].total_endpoints_tested;
+                document.getElementById('FUNCTIONAL_SCENARIOS_NUMBER').innerHTML = sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].total_scenarios;
+                if(sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].contract_test_coverage == "N/A"){
+                    document.getElementById('CONTRACT_PERCENTAGE').innerHTML = sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].contract_test_coverage;
+                }
+                else{
+                    document.getElementById('CONTRACT_PERCENTAGE').innerHTML = sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].contract_test_coverage + "%";
+                }
+                document.getElementById('ENDPOINTS_NUMBER').innerHTML = sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].total_endpoints_tested;
+                
+                document.getElementById('ALL-FEATURES-AND-SCENARIOS').innerHTML = ""
+                for(var index_feature = 0; index_feature < sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features.length ; index_feature++)
+                {
+                    document.getElementById('ALL-FEATURES-AND-SCENARIOS').insertAdjacentHTML('beforeend', "<h4 id='PAINEL-SCENARIOS" + index_feature + "' class='panel-title'>");
+                    document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "<a id='VIEW-COLLAPSE" + index_feature + "' data-toggle='collapse' href='#collapse" + index_feature +"'>");
+                    document.getElementById('VIEW-COLLAPSE' + index_feature).insertAdjacentHTML('beforeend', "<p class='m-t-30 f-w-600'>" 
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].feature_name 
+                                        + " (" + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].number_scenarios_implemented 
+                                        + "/" + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].total_number_scenarios 
+                                        + ") <span class='pull-right'>" + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage + "%</span></p>");
+                    
+                    document.getElementById('VIEW-COLLAPSE' + index_feature).insertAdjacentHTML('beforeend', "<div id='PROGRESS-SCENARIOS" + index_feature +  "' class='progress'>");
+                    if(sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage >= dod_functional_test && sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage < 99){
+                        document.getElementById('PROGRESS-SCENARIOS' + index_feature).innerHTML = "<div role='progressbar' style='width:" 
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage 
+                                        + "%; height:8px;' class='progress-bar bg-info wow animated progress-animated'> <span class='sr-only'>"
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage + "% Complete</span> </div>"
+                    }
+                    else if(sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage < dod_functional_test){
+                        document.getElementById('PROGRESS-SCENARIOS' + index_feature).innerHTML = "<div role='progressbar' style='width:" 
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage 
+                                        + "%; height:8px;' class='progress-bar bg-danger wow animated progress-animated'> <span class='sr-only'>"
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage + "% Complete</span> </div>"
+                    }
+                    else {
+                        document.getElementById('PROGRESS-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "<div role='progressbar' style='width:" 
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage 
+                                        + "%; height:8px;' class='progress-bar bg-success wow animated progress-animated'> <span class='sr-only'>"
+                                        + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].coverage + "% Complete</span> </div>");
+                    }
+                        
+                    document.getElementById('PROGRESS-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "</div>");
+                    document.getElementById('VIEW-COLLAPSE' + index_feature).insertAdjacentHTML('beforeend', "</a>");
+                    
+                    document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "<div id='collapse" + index_feature + "' class='panel-collapse collapse'>")
+                    
+                    if("scenarios_implemented" in sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature]){
+
+                        if(sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].scenarios_implemented.length > 0){
+                            for(var j = 0; j < sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].scenarios_implemented.length; j++){
+                                document.getElementById('collapse' + index_feature).insertAdjacentHTML('beforeend', "<div style='color:Green' class='panel'>" 
+                                            + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].scenarios_implemented[j] + "</div>")
+                            }
+                            
+                        }
+                    }
+
+                    if("scenarios_not_implemented" in sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature]){
+                        if(sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].scenarios_not_implemented.length > 0)
+                        {
+                            for(var j = 0; j < sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].scenarios_not_implemented.length; j++){
+                                document.getElementById('collapse' + index_feature).insertAdjacentHTML('beforeend', "<div style='color:lightCoral' class='panel'>" 
+                                            + sprintsTableJSON[key].platforms[Object.keys(sprintsTableJSON[key].platforms)[i]].features[index_feature].scenarios_not_implemented[j] +"</div>")
+                            }
+                        }
+                    }
+                    
+                    document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "</div>");
+                    document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "</h4>");
+
+                }
             }
         }
 
     }
 
     var set_name = function(){
-        // window.location.reload()
-        var key = this.id;
-        for(var i = 0; i < Object.keys(sprintsTableJSON[this.id].platforms).length; i ++){
-            document.getElementById('REPORT_PLATFORM_NAME').insertAdjacentHTML('beforeend', "<li><a id='menu-"+ Object.keys(sprintsTableJSON[this.id].platforms)[i] +"' href='javascript:void(0)'>"+ Object.keys(sprintsTableJSON[this.id].platforms)[i] +"</a></li>")
-            document.getElementById('menu-' + Object.keys(sprintsTableJSON[this.id].platforms)[i]).addEventListener("click", function () {
+        document.getElementById('REPORT_PLATFORM_NAME').innerHTML = ""
+        var key = this.id.split(":")[1];
+        for(var i = 0; i < Object.keys(sprintsTableJSON[key].platforms).length; i ++){
+            document.getElementById('REPORT_PLATFORM_NAME').insertAdjacentHTML('beforeend', "<li><a id='menu-"+ Object.keys(sprintsTableJSON[key].platforms)[i] +"' href='javascript:void(0)'>"+ Object.keys(sprintsTableJSON[key].platforms)[i] +"</a></li>")
+            document.getElementById('menu-' + Object.keys(sprintsTableJSON[key].platforms)[i]).addEventListener("click", function () {
                 set_dashboard(this.id, key);
             }, false);
-            // if(fileName.includes(generalDatas.platforms[i].platform)){
-            //     document.getElementById('FUNCTIONAL_PERCENTAGE').innerHTML = generalDatas.platforms[i].functional_coverage + "%";
-            //     document.getElementById('FUNCTIONAL_SCENARIOS_NUMBER').innerHTML = generalDatas.platforms[i].total_scenarios;
-            //     if(generalDatas.platforms[i].contract_test_coverage == "N/A"){
-            //         document.getElementById('CONTRACT_PERCENTAGE').innerHTML = generalDatas.platforms[i].contract_test_coverage;
-            //     }
-            //     else{
-            //         document.getElementById('CONTRACT_PERCENTAGE').innerHTML = generalDatas.platforms[i].contract_test_coverage + "%";
-            //     }
-            //     document.getElementById('ENDPOINTS_NUMBER').innerHTML = generalDatas.platforms[i].total_endpoints_tested;
-                
-                // console.log(Object.keys(sprintsTableJSON[this.id].platforms)[0]);
-            }
+        }
+    
     }
+
     for(var i = 0; i < keys.length; i++)
     {
-        document.getElementById(keys[i]).onclick = set_name
+        document.getElementById("name:" + keys[i]).onclick = set_name
+        document.getElementById("img:" + keys[i]).onclick = set_name
     }
     
-
-    // let keys = Object.keys(sprintsTableJSON).sort()
-
-    
-    // for(var i = 0; i < generalDatas.platforms.length; i ++){
-    //     if(fileName.includes(generalDatas.platforms[i].platform)){
-    //         document.getElementById('FUNCTIONAL_PERCENTAGE').innerHTML = generalDatas.platforms[i].functional_coverage + "%";
-    //         document.getElementById('FUNCTIONAL_SCENARIOS_NUMBER').innerHTML = generalDatas.platforms[i].total_scenarios;
-    //         if(generalDatas.platforms[i].contract_test_coverage == "N/A")
-    //         {
-    //             document.getElementById('CONTRACT_PERCENTAGE').innerHTML = generalDatas.platforms[i].contract_test_coverage;
-    //         }
-    //         else{
-    //             document.getElementById('CONTRACT_PERCENTAGE').innerHTML = generalDatas.platforms[i].contract_test_coverage + "%";
-    //         }
-    //         document.getElementById('ENDPOINTS_NUMBER').innerHTML = generalDatas.platforms[i].total_endpoints_tested;
-    //         for(var index_feature = 0; index_feature < generalDatas.platforms[i].features.length ; index_feature++)
-    //         {
-    //             document.getElementById('ALL-FEATURES-AND-SCENARIOS').insertAdjacentHTML('beforeend', "<h4 id='PAINEL-SCENARIOS" + index_feature + "' class='panel-title'>");
-    //             document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "<a id='VIEW-COLLAPSE" + index_feature + "' data-toggle='collapse' href='#collapse" + index_feature +"'>");
-    //             document.getElementById('VIEW-COLLAPSE' + index_feature).insertAdjacentHTML('beforeend', "<p class='m-t-30 f-w-600'>" 
-    //                                 + generalDatas.platforms[i].features[index_feature].feature_name 
-    //                                 + " (" + generalDatas.platforms[i].features[index_feature].number_scenarios_implemented 
-    //                                 + "/" + generalDatas.platforms[i].features[index_feature].total_number_scenarios 
-    //                                 + ") <span class='pull-right'>" + generalDatas.platforms[i].features[index_feature].coverage + "%</span></p>");
-                
-    //             document.getElementById('VIEW-COLLAPSE' + index_feature).insertAdjacentHTML('beforeend', "<div id='PROGRESS-SCENARIOS" + index_feature +  "' class='progress'>");
-    //             if(generalDatas.platforms[i].features[index_feature].coverage >= dod_functional_test && generalDatas.platforms[i].features[index_feature].coverage < 99){
-    //                 document.getElementById('PROGRESS-SCENARIOS' + index_feature).innerHTML = "<div role='progressbar' style='width:" 
-    //                                 + generalDatas.platforms[i].features[index_feature].coverage 
-    //                                 + "%; height:8px;' class='progress-bar bg-info wow animated progress-animated'> <span class='sr-only'>"
-    //                                 + generalDatas.platforms[i].features[index_feature].coverage + "% Complete</span> </div>"
-    //             }
-    //             else if(generalDatas.platforms[i].features[index_feature].coverage < dod_functional_test){
-    //                 document.getElementById('PROGRESS-SCENARIOS' + index_feature).innerHTML = "<div role='progressbar' style='width:" 
-    //                                 + generalDatas.platforms[i].features[index_feature].coverage 
-    //                                 + "%; height:8px;' class='progress-bar bg-danger wow animated progress-animated'> <span class='sr-only'>"
-    //                                 + generalDatas.platforms[i].features[index_feature].coverage + "% Complete</span> </div>"
-    //             }
-    //             else {
-    //                 document.getElementById('PROGRESS-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "<div role='progressbar' style='width:" 
-    //                                 + generalDatas.platforms[i].features[index_feature].coverage 
-    //                                 + "%; height:8px;' class='progress-bar bg-success wow animated progress-animated'> <span class='sr-only'>"
-    //                                 + generalDatas.platforms[i].features[index_feature].coverage + "% Complete</span> </div>");
-    //             }
-                    
-    //             document.getElementById('PROGRESS-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "</div>");
-    //             document.getElementById('VIEW-COLLAPSE' + index_feature).insertAdjacentHTML('beforeend', "</a>");
-                
-    //             document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "<div id='collapse" + index_feature + "' class='panel-collapse collapse'>")
-                
-    //             if(generalDatas.platforms[i].features[index_feature].scenarios_implemented.length > 0){
-    //                 for(var j = 0; j < generalDatas.platforms[i].features[index_feature].scenarios_implemented.length; j++){
-    //                     document.getElementById('collapse' + index_feature).insertAdjacentHTML('beforeend', "<div style='color:Green' class='panel'>" 
-    //                                 + generalDatas.platforms[i].features[index_feature].scenarios_implemented[j] + "</div>")
-    //                 }
-                    
-    //             }
-    //             if(generalDatas.platforms[i].features[index_feature].scenarios_not_implemented.length > 0)
-    //             {
-    //                 for(var j = 0; j < generalDatas.platforms[i].features[index_feature].scenarios_not_implemented.length; j++){
-    //                     document.getElementById('collapse' + index_feature).insertAdjacentHTML('beforeend', "<div style='color:lightCoral' class='panel'>" 
-    //                                 + generalDatas.platforms[i].features[index_feature].scenarios_not_implemented[j] +"</div>")
-    //                 }
-    //             }
-                
-    //             document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "</div>");
-    //             document.getElementById('PAINEL-SCENARIOS' + index_feature).insertAdjacentHTML('beforeend', "</h4>");
-
-    //         }
     //         var sprintMetricDatas = JSON.parse(data_metrics);
     //         for(var index_sprint = sprintMetricDatas.metrics.length - 1; index_sprint >= 0; index_sprint--){
     //             if(sprintMetricDatas.metrics[index_sprint].platform == generalDatas.platforms[i].platform){
--- template/js/load-datas/load-sprints-table.js
+++ template/js/load-datas/load-sprints-table.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
     let keys = Object.keys(sprintsTableJSON).sort()
     for(var i = 0; i < keys.length; i++)
     {
-        document.getElementById('table-of-sprints-name').insertAdjacentHTML('beforeend', '<ul id="elements'+ i + '" class="list-style-none">')
-        document.getElementById('elements' + i).insertAdjacentHTML('beforeend', '<li><a id="' + keys[i] + '" href="javascript:void(0)"><i class="fa fa-check text-success"></i> ' + sprintsTableJSON[keys[i]].sprint_name + '</a></li>')
-        document.getElementById('table-of-sprints-name').insertAdjacentHTML('beforeend', '</ul>')
+        document.getElementById('sprint-notifications').insertAdjacentHTML('beforeend', '<a id="element' + i + '" href="#">')
+        document.getElementById('element' + i).insertAdjacentHTML('beforeend','<div id="img:' + keys[i] + '" class="btn btn-success btn-circle m-r-10"><i class="fa fa-link"></i></div>')
+        document.getElementById('element' + i).insertAdjacentHTML('beforeend','<div id="name:' + keys[i] + '" class="mail-contnet"><h5>' + sprintsTableJSON[keys[i]].sprint_name + '</h5>')
+        document.getElementById('element' + i).insertAdjacentHTML('beforeend','</div>')
+        document.getElementById('sprint-notifications').insertAdjacentHTML('beforeend','</a>')
     }
 
 })
